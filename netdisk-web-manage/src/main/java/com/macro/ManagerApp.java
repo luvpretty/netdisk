34653e4fad034a4ef3fd9e35614933f34664fa8c
luvpretty <975551732@qq.com>
2021-01-24 21:31:28 +0800

    #初始化controller工程环境的搭建,引入service-api

---
+++ netdisk-web-manage/src/main/java/com/macro/ManagerApp.java
@@ -0,0 +1,14 @@
+package com.macro;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerApp.class, args);
+	}
+}
+
---
+++ netdisk-web-perpc/src/main/java/com/macro/PerpcApp.java
@@ -0,0 +1,14 @@
+package com.macro;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerpcApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PerpcApp.class, args);
+	}
+}
+
