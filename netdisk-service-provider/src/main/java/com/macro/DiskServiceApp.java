beaaea7c17a4e2941eaf128b89d96a368535aec6
luvpretty <975551732@qq.com>
2021-01-24 21:57:20 +0800

    #集成nacos配置中心、nacos注册中心

--- netdisk-service-provider/src/main/java/com/macro/DiskServiceApp.java
+++ netdisk-service-provider/src/main/java/com/macro/DiskServiceApp.java
@@ -2,8 +2,10 @@ package com.macro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DiskServiceApp{
     public static void main(String[] args) {
         SpringApplication.run(DiskServiceApp.class, args);
