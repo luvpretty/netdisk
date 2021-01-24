591df1948ebde4b9e97d5faea1cbc22342691b3d
luvpretty <975551732@qq.com>
2021-01-24 21:20:21 +0800

    #初始化service 工程环境,集成jpa

---
+++ netdisk-service-provider/src/main/java/com/macro/DiskServiceApp.java
@@ -0,0 +1,11 @@
+package com.macro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiskServiceApp{
+    public static void main(String[] args) {
+        SpringApplication.run(DiskServiceApp.class, args);
+    }
+}
